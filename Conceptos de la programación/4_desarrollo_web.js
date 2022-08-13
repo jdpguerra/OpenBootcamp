46ee0ae7b3b08711590303216eb821a6372e22d3
Jose Guerra <58611147+jdpguerra@users.noreply.github.com>
2022-08-12 22:53:26 -0300

    vert
    v1

--- Conceptos de la programación/4_desarrollo_web.js
+++ Conceptos de la programación/4_desarrollo_web.js
@@ -12,8 +12,45 @@ if(introduccion == "4.1"){
 //mayor punto de calculo esta en el backend no dependiendo de la conexion del provedor
 
 //Full stack
-//
+//construir una application de zero a todo
+//moviendo toda la applicacion front end y backend
+
 
 }
 else if(introduccion == "4.2"){
+//lenguajes de servidor
+//perl
+//java
+//php
+//nodeJs
+//ruby
+
+//lenguajes del client
+//html5
+//css3
+//javascript
+//typescript
+//ajax
+
+}
+else if(introduccion == "4.3"){
+//Content management system CMS
+//tiene su proprio software con front end y backend
+
+//programados en php y javascript
+//wordpress
+//joomla
+//drupal
+//magento
+//prestshop
+
+//html+javascript
+//shopify
+
+//python +javascript
+//odoo > CRM > client relationship manager
+
+}
+else if(introduccion == "4.4"){
+
 }
